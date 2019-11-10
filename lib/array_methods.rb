434e1b057d7229093455df9d8c4200dfc6a20dbf
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-10 04:13:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,13 @@
-
+def find_element_index(array, value_to_find)
+  count = 0
+ while count < array.length do 
+  if array[count] == value_to_find
+  return count
+ end 
+ count += 1
+end
+ nil
+ end
  
  def find_element_index(array, value_to_find)
    array.length.times do |count|
@@ -7,6 +16,8 @@
      end
      nil
    end
+   
+ end
 
 def find_max_value(array)
   return array.max
