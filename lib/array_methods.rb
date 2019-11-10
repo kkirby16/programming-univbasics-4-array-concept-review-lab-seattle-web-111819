1c606fcd33df7bd039c8e92afbf78c30027dd2ad
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-10 04:13:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,16 +8,6 @@ def find_element_index(array, value_to_find)
 end
  nil
  end
- 
- def find_element_index(array, value_to_find)
-   array.length.times do |count|
-     if array[count] == value_to_find
-       return count
-     end
-     nil
-   end
-   
- end
 
 def find_max_value(array)
   return array.max
